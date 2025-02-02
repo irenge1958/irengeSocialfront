83c792cf3b167f10f1054170206062342f641929
Irenge Bahizire <eshibahizire@gmail.com>
2025-02-02 20:57:23 +0800

    2profile

--- src/component/feed/feed.js
+++ src/component/feed/feed.js
@@ -8,7 +8,7 @@ import { useLocation} from 'react-router-dom';
 import apiClient from '../../apiclient'
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
-
+import { useMediaQuery } from 'react-responsive';
 const Feed =() => {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
@@ -19,6 +19,7 @@ const Feed =() => {
     const [updateduser,setupdateduser]=useState(user)
     const [posts, setPosts] = useState([]);
     const [guest,setguest]=useState({})
+    const isMobile = useMediaQuery({ maxWidth: 767 });
     const follow=async()=>{
       await apiClient.put(`users/follow/${user._id}`,{id:id,profilepicture:user.profilepicture,username:user.username})
       const newuser=await apiClient.get(`users/${user._id}`)
@@ -104,33 +105,49 @@ const Feed =() => {
       // };
     }, [id]);
     return ( <div className="wrapperfeed">
-<div className="descrie" style={{marginTop:'-50px',marginBottom:'10px'}}>
-                <p>City:{user.city?user.city:'--'}</p>
-                <p>From:{user.from?user.from:'--'}</p>
-                <p>Went to:{user.school?user.school:'--'}</p>
-                <p>relationship:{user.relationship?user.relationship:'--'}</p>
-         </div>
-      {user._id!==id && <div className="myfolandunfoli" style={{ display: "flex",
+      {isMobile && <> {id && <div className="descrie" style={{marginTop:'-50px',marginBottom:'10px'}}>
+      <p>City: {user._id === id ? (user.city ? user.city : "--") : (guest.city ? guest.city : "--")}</p>
+      <p>From: {user._id === id ? (user.from ? user.from : "--") : (guest.from ? guest.from : "--")}</p>
+      <p>Went to: {user._id === id ? (user.school ? user.school : "--") : (guest.school ? guest.school : "--")}</p>
+      <p>status: {user._id === id ? (user.relationship ? user.relationship : "--") : (guest.relationship ? guest.relationship : "--")}</p>
+
+               
+         </div>} 
+      {user._id!==id && id && <div className="myfolandunfoli" style={{ display: "flex",
     justifyContent: "center", // Centers horizontally
     alignItems: "center", // Centers vertically
   
   }}>
     
-      <div className="descrie" style={{display:'flex',gap:'20px'}}>
+      <div className="descrie" style={{display:'flex',gap:'10px'}}>
       {updateduser.followings.includes(id)?<button style={{display:'flex',gap:'2px'}} onClick={unfollow}>Unfollow <RemoveIcon /></button>:<button style={{display:'flex',gap:'2px'}} onClick={follow}>Follow <AddIcon /></button>}  
-                <p>Followers:{id?guest.followers?.length:updateduser.followers?.length}</p>
-                <p>Followings:{id?guest.followings?.length:updateduser.followings?.length}</p>
+                <p>Followers:{guest.followers?.length}</p>
+                <p>Followings:{guest.followings?.length}</p>
                 </div>
       
-            </div>}  
-            <hr style={{
+            </div>} 
+            {user._id===id && <div className="myfolandunfoli" style={{ display: "flex",
+    justifyContent: "center", // Centers horizontally
+    alignItems: "center", // Centers vertically
+  
+  }}>
+    
+      <div className="descrie" style={{display:'flex',gap:'10px'}}>
+      
+                <p>Followers:{updateduser.followers?.length}</p>
+                <p>Followings:{updateduser.followings?.length}</p>
+                </div>
+      
+            </div>}   
+            { id &&    <hr style={{
  height: "3px",
  width: "95%",
  backgroundColor: "#b0b0b0", // Light gray
  border: "none", // Remove default border
  margin: "20px auto", // Center horizontally
  borderRadius: "5px" // Smooth edges
-}}></hr>
+}}></hr>}</>} 
+
      {(username===user.username || username==null) && <Post />}
  {posts.length>0 ? posts.map(post => (
   <Posted key={post._id}  post={post} />
