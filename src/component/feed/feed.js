e9cae49a9b9737bbceaee38ba49cb397729f7d1d
Irenge Bahizire <eshibahizire@gmail.com>
2025-02-18 16:29:43 +0800

    videoss

--- src/component/feed/feed.js
+++ src/component/feed/feed.js
@@ -54,7 +54,10 @@ const Feed =() => {
                 } if(video){
               
                   reponse=response.data.filter((a)=>{return getFileExtension(a.postpicture)==='mp4'})
-                 
+                  if(reponse.length<10){
+                    reponse = await apiClient.get('post/randomv');
+                    reponse =reponse.data.filter((a)=>{return getFileExtension(a.postpicture)==='mp4'})
+                  }
                   setPosts(reponse.sort((p1,p2)=>{
                     return new Date(p2.createdAt)-new Date(p1.createdAt)
                   }));
